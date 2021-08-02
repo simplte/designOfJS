a8e0dadb4eff058690c6254c793c4a48c4586d82
bqc <6575734@qq.com>
2021-08-02 22:33:27 +0800

    feat: 🎸 了解不纯函数对编码过程中造成的潜在问题

--- 函数式编程/组合函数/index.js
+++ 函数式编程/组合函数/index.js
@@ -103,3 +103,50 @@ function pipe(...fns) {
 // 5:代码抽离的目的除了方便重用，还有就是将相关逻辑独立起来，让我们在关注一部分逻辑时不被其他的逻辑影响到
 // 函数组合是一种定义函数的模式，该函数将一个函数调用的输出路由到另一个函数调用，并将其输出路由到另一个函数调用，以此类推。
 // 个人理解：一个函数的输出作为另外一个函数的输入
+
+function test(obj) {
+    obj.a = 1;
+    return obj.a +1;
+}
+let obj1 = {a: 2}
+test(obj1);
+console.log(obj1)
+
+// 6:由于handlefn 是一个不纯的方法，这种写法会导致 test1被改变
+let test1 = {a:1,b:2}
+function handlefn(obj) {
+    obj.a +=1;
+    return obj.a + obj.b
+}
+console.log(handlefn(test1))
+console.log(test1)
+
+// 6.1  使用const 一样会导致test2 数据发生改变
+const test2 =  {a:1,b:2}
+console.log(handlefn(test2))
+console.log(test2)
+// 6.2 解决不纯函数带来的问题 解决不纯函数带来的不可预知的问题的方式
+//  使用Object.freeze() 来声明一个浅层不可改变的对象
+const test3 =  Object.freeze({a:1,b:2})
+console.log(handlefn(test3))
+console.log(test3)
+// 6.3 使用 Object.freeze() 如果声明一个深层的对象
+// ，并且使用一个不纯的函数去修改深层的数据 
+// 使用Object.freeze() 声明的对象一样会被改变
+let test4 = Object.freeze({a:1,b:{c:{d:2}}})
+function handlefn(obj) {
+    obj.b.c.d +=1;
+    return obj.a + obj.b.c.d
+}
+console.log(handlefn(test4))
+console.log(test4)
+// 6.4 彻底解决此类问题，
+// 个人理解：改写不纯函数，1：使用深拷贝 拷贝参数值  2：使用原型方法声明一个新值（本质上同第一点相同）
+let test4 = {a:1,b:{c:{d:2}}}
+function handlefn(obj) {
+    let args = JSON.parse(JSON.stringify(obj))
+    args.b.c.d +=1;
+    return args.a + args.b.c.d
+}
+console.log(handlefn(test4))
+console.log(test4)
