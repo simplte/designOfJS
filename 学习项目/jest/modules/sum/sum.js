3d963fd991595bd1f9620c3127cd854bffa28f90
bqc <6575734@qq.com>
2021-09-05 14:44:18 +0800

    feat: 🎸 学习jest使用及配置

---
+++ 学习项目/jest/modules/sum/sum.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => a + b;
+export default sum;
+
+// const sum = (a, b) =>{ return a + b};
+// module.exports =sum;
---
+++ 学习项目/jest/test/unit/sum.test.js
@@ -0,0 +1,8 @@
+import sum from '../../modules/sum/sum.js'
+test("1+2=3", () => {
+    expect(sum(1,2)).toBe(3);
+})
+// const sum = require('../../modules/sum/sum.js')
+// test("1+2=3", () => {
+//     expect(sum(1,2)).toBe(3);
+// })
