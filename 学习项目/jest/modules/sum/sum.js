e76982cafca6fad3034b96681e0d0b2df2c5f748
bqc <6575734@qq.com>
2021-09-05 19:09:31 +0800

    feat: 🎸 jest支持ts

--- 学习项目/jest/modules/sum/sum.js
+++
@@ -1,5 +0,0 @@
-const sum = (a, b) => a + b;
-export default sum;
-
-// const sum = (a, b) =>{ return a + b};
-// module.exports =sum;
---
+++ 学习项目/jest/test/unit/sum.test.ts
@@ -0,0 +1,4 @@
+import sum from '../../modules/sum/sum'
+test("1+2=3", () => {
+    expect(sum(1,2)).toBe(3);
+})
