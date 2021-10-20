024754de2f993551726f94ccd972f3b6dfafd3b6
bqc <6575734@qq.com>
2021-10-20 22:41:44 +0800

    feat: 🎸 prompts

--- create-vue/prompts.js
+++ create-vue/prompts.js
@@ -9,5 +9,20 @@ let result = await prompts([
         initial: defaultProjectName,
         onState: (state) => (targetDir = String(state.value).trim() || defaultProjectName)
       },
+      {
+        name: 'projectName',
+        type: targetDir ? null : 'text',
+        message: 'Project name:',
+        initial: defaultProjectName,
+        onState: (state) => (targetDir = String(state.value).trim() || defaultProjectName)
+      },
+      {
+        name: 'needsJsx',
+        type:  'toggle',
+        message: 'Add JSX Support?',
+        initial: false,
+        active: 'Yes',
+        inactive: 'No'
+      },
 ])
 console.log(result)
