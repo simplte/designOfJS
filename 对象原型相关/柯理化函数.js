9feeeb5f09d64fe43404d93b7ec02f4b94285335
bqc <6575734@qq.com>
2021-12-22 22:52:42 +0800

    feat: 🎸 八股文

--- 对象原型相关/柯理化函数.js
+++ 对象原型相关/柯理化函数.js
@@ -16,3 +16,24 @@ function curry(fn, args) {
     }
   };
 }
+
+function curry(fn, args) {
+  // 获取函数的参数数量
+  const len = fn.length;
+  // 第一次执行curry时传入的参数数量
+  args = args || [];
+  return function () {
+    // 通过slice复制之前已经传的参数
+    var _args = args.slice(0);
+    // 获取再次执行时新传入得参数
+    for (var i = 0; i < arguments.length; i++) {
+      _args.push(arguments[i]);
+    }
+    // 参数数量不够，递归执行curry 将函数和 新增加的参数传给curry
+    if (_args.length < len) {
+      return curry.call(this, fn, _args);
+    } else {
+      return fn.apply(this, _args);
+    }
+  };
+}
