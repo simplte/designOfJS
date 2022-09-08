1bfdb99d9c13829a9db637e4930f62a9c2361b0a
bqc <6575734@qq.com>
2022-09-08 14:34:30 +0800

    docs:babel

---
+++ babel/babel-handhook/src/index.js
@@ -0,0 +1,11 @@
+const test = () => {
+  console.log('123123');
+};
+class Man {
+  constructor() {
+    console.log('123');
+  }
+  getName() {
+    return 'man';
+  }
+}
---
+++ babel/babel-handhook/transform/index.js
@@ -0,0 +1,12 @@
+const babel = require('babel-core');
+const fs = require('fs');
+const path = require('path');
+const testFilePath = path.join(__dirname, '../src/index.js');
+console.log(testFilePath);
+const testCode = fs.readFileSync(testFilePath, 'utf8');
+console.log(testCode);
+// const { code, map, ast } = babel.transform('() => { console.log(123) } ');
+// const { code, map, ast } = babel.transform(testCode);
+const { code, map, ast } = babel.transformFileSync(testFilePath);
+
+console.log(code);
